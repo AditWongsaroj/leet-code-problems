521f8688b8635cda29ffb321a128e09e5ca71713
Adit <aditwongsaroj@gmail.com>
2024-01-26 23:17:43 -0600

    p2 python3 added with tests

--- 2-addTwoNumbers/JS/addTwoNumbers.test.js
+++ 2-addTwoNumbers/JS/addTwoNumbers.test.js
@@ -17,31 +17,35 @@ function listnodeconstructor(array) {
 test("zeros", () => {
   let l1 = listnodeconstructor([0]);
   let l2 = listnodeconstructor([0]);
-  let result = listnodeconstructor([0]);
+  let want = listnodeconstructor([0]);
+  let got = addTwoNumbers(l1, l2);
 
-  expect(addTwoNumbers(l1, l2)).toStrictEqual(result);
+  expect(got).toStrictEqual(want);
 });
 
 test("simple carry", () => {
   let l1 = listnodeconstructor([2, 4, 3]);
   let l2 = listnodeconstructor([5, 6, 4]);
-  let result = listnodeconstructor([7, 0, 8]);
+  let want = listnodeconstructor([7, 0, 8]);
+  let got = addTwoNumbers(l1, l2);
 
-  expect(addTwoNumbers(l1, l2)).toStrictEqual(result);
+  expect(got).toStrictEqual(want);
 });
 
 test("multi carry", () => {
   let l1 = listnodeconstructor([9, 9, 9]);
   let l2 = listnodeconstructor([9, 9, 9]);
-  let result = listnodeconstructor([8, 9, 9, 1]);
+  let want = listnodeconstructor([8, 9, 9, 1]);
+  let got = addTwoNumbers(l1, l2);
 
-  expect(addTwoNumbers(l1, l2)).toStrictEqual(result);
+  expect(got).toStrictEqual(want);
 });
 
 test("size difference, multi carry", () => {
   let l1 = listnodeconstructor([9, 9, 9, 9, 9, 9, 9]);
   let l2 = listnodeconstructor([9, 9, 9, 9]);
-  let result = listnodeconstructor([8, 9, 9, 9, 0, 0, 0, 1]);
+  let want = listnodeconstructor([8, 9, 9, 9, 0, 0, 0, 1]);
+  let got = addTwoNumbers(l1, l2);
 
-  expect(addTwoNumbers(l1, l2)).toStrictEqual(result);
+  expect(got).toStrictEqual(want);
 });
